209e7b809845250475cd4aa866e180d92574819b
Roman Polník <roman.polnik@skautocont.onmicrosoft.com>
2026-06-30 13:14:24 +0200

    first page

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+"""URL configuration for config project."""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+]
